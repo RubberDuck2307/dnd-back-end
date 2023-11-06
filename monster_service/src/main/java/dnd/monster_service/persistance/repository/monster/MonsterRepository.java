8d638e88ca40258c76e6c7fffaa3b525242731b7
Jan Metela <janmetela@seznam.cz>
2023-11-06 14:38:56 +0100

    MonsterService deleting outdated files

--- monster_service/src/main/java/dnd/monster_service/persistance/repository/monster/MonsterRepository.java
+++ monster_service/src/main/java/dnd/monster_service/persistance/repository/monster/MonsterRepository.java
@@ -18,10 +18,10 @@ public interface MonsterRepository extends JpaRepository<Monster, Long> {
     Map<Float, List<Monster>> getMonstersByCrAndAmount(Map<Float, Integer> crs);
 
     Map<Float, List<Monster>> getMonstersByCrAmountAndMonsterGroupId(Map<Float, Integer> crs,
-                                                                          Long monsterGroupId);
-    Map<Float, Integer> getAmountOfMonstersByCrAndMonsterGroup(Map<Float, Integer> crs, Long monsterGroupId);
+                                                                          long monsterGroupId);
+    Map<Float, Integer> getAmountOfMonstersByCrAndMonsterGroup(Map<Float, Integer> crs, long monsterGroupId);
     @Query("select count(m) from Monster m join m.monsterGroups mg where mg.id = :id")
-    int countByMonsterGroupId(Long id);
+    int countByMonsterGroupId(long id);
     @Query("SELECT distinct m.cr FROM MonsterGroup mg JOIN mg.monsters m WHERE mg.id = :id")
-    List<Float> getAllCrWhereMonsterGroupsContaining_Id(Long id);
+    List<Float> getAllCrWhereMonsterGroupsContaining_Id(long id);
 }
